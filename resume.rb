bebb753abef44b1ef973c50ac1f568953ce2c582
Dennis Collinson <dennis.collective@gmail.com>
2013-06-06 19:00:01 -0700

    moar better grammar

--- resume.rb
+++ resume.rb
@@ -148,8 +148,8 @@ SNARK
 <<ABSTRACT
 # In Brief:
 
-I enjoy the challenge of working with human beings to create software the fills a real need. I've been making websites
-since shortly after I was born, to that end, I am as interested in human aspects as I am the technical side of things.
+I enjoy the challenge of working with human beings to create software that fills a real need. I've been making websites
+since shortly after I was born; to that end, I am as interested in human aspects as I am the technical side of things.
 I try to work sensibly and sustainably with teams who take pride in their work.
 
 ## I am not afraid of the words:
